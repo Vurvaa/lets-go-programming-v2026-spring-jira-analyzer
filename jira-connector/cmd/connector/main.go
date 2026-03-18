15ba08f2c55fab2b085600d67f8541aae679a4eb
Tuc0Sa1amanka <sparowj765@gmail.com>
2026-03-18 12:58:10 +0300

    perf(connector): enhance existing functions with concurrent loading and rate limit resilience

--- jira-connector/cmd/connector/main.go
+++ jira-connector/cmd/connector/main.go
@@ -1,60 +1,20 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"jira-connector/internal/connector"
-	"jira-connector/internal/dataTransformer"
-	"jira-connector/internal/pusher"
 	"log"
 )
 
 func main() {
 	url := "https://issues.apache.org"
-	projectPtr, err := connector.GetProject(url, "AAR")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(projectPtr)
-
-	project := []connector.Project{*projectPtr}
-	err = connector.GetIssues(url, project, 1000)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	issues, err := dataTransformer.ParseIssues(project)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println(issues)
-
 	projects, err := connector.GetProjects(url)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	if err = connector.GetIssues(url, projects, 1000); err != nil {
-		log.Fatal(err)
+	for _, project := range projects {
+		err = connector.GetIssues(url, &project, 200, 30)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-
-	parsedIssues, err := dataTransformer.ParseIssues(projects)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	connectionString := "postgres://pguser:pgpwd@jira_postgres:5432/jira_db?sslmode=disable"
-	store, err := pusher.NewStorage(connectionString)
-	if err != nil {
-		log.Fatal("Failed to connect to store:", err)
-	}
-	defer store.Close()
-
-	ctx := context.Background()
-	if err := store.SaveAll(ctx, parsedIssues, projects); err != nil {
-		log.Fatal("Failed to save data:", err)
-	}
-
-	log.Println("Successfully saved data")
 }
