af9631491f9519db54fc7161f117c01461157701
Vurvaa <egor.vurva@gmail.com>
2026-04-02 12:44:04 +0300

    refactor(api-server): move helper functions to handlers package

--- jira-connector/internal/apiServer/handlers/handlers.go
+++ jira-connector/internal/apiServer/handlers/handlers.go
@@ -1,1 +1,74 @@
 package handlers
+
+import (
+	"jira-connector/internal/apiServer/projectModels"
+	"jira-connector/internal/connector"
+	"math"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func GetProjectResponse(page, limit int, projects []projectModels.Project) projectModels.ProjectResponse {
+	projectsCount := len(projects)
+	startIndex := (page - 1) * limit
+	endIndex := startIndex + limit
+	if endIndex >= len(projects) {
+		endIndex = len(projects)
+	}
+
+	return projectModels.ProjectResponse{
+		Projects: projects[startIndex:endIndex],
+		PageInfo: projectModels.PageInfo{
+			CurrentPage:   page,
+			PageCount:     int(math.Ceil(float64(projectsCount) / float64(limit))),
+			ProjectsCount: projectsCount,
+		},
+	}
+}
+
+func HandleProjects(url, search string) ([]projectModels.Project, error) {
+	projects, err := connector.GetProjects(url)
+	if err != nil {
+		return nil, err
+	}
+
+	var responseProjects []projectModels.Project
+
+	for _, project := range projects {
+		isCorrectName := strings.Contains(strings.ToLower(project.ProjectName), strings.ToLower(search))
+		if isCorrectName {
+			responseProject := projectModels.Project{
+				ProjectId:   project.ProjectId,
+				ProjectName: project.ProjectName,
+				Existence:   false}
+
+			responseProjects = append(responseProjects, responseProject)
+		}
+	}
+
+	return responseProjects, nil
+}
+
+func ParseProjectParameters(request *http.Request) (int, int, string) {
+	limit := 20
+	page := 1
+	search := ""
+
+	limitPrm := request.URL.Query().Get("limit")
+	if len(limitPrm) != 0 {
+		limit, _ = strconv.Atoi(limitPrm)
+	}
+
+	pagePrm := request.URL.Query().Get("page")
+	if len(pagePrm) != 0 {
+		page, _ = strconv.Atoi(pagePrm)
+	}
+
+	searchPrm := request.URL.Query().Get("search")
+	if len(searchPrm) != 0 {
+		search = searchPrm
+	}
+
+	return limit, page, search
+}
--- jira-connector/internal/apiServer/server.go
+++ jira-connector/internal/apiServer/server.go
@@ -5,13 +5,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"math"
 	"net/http"
-	"strconv"
-	"strings"
 	"time"
 
-	"jira-connector/internal/apiServer/projectModels"
+	"jira-connector/internal/apiServer/handlers"
 	"jira-connector/internal/apiServer/serverConfig"
 	"jira-connector/internal/configReader"
 	"jira-connector/internal/connector"
@@ -108,16 +105,16 @@ func (server *Server) projects(writer http.ResponseWriter, request *http.Request
 		return
 	}
 
-	limit, page, search := parseProjectParameters(request)
+	limit, page, search := handlers.ParseProjectParameters(request)
 
-	projects, err := handleProjects(search)
+	projects, err := handlers.HandleProjects(server.config.Repository, search)
 	if err != nil {
 		http.Error(writer, fmt.Sprintf("error while downloading list of projects: %v", err), http.StatusBadRequest)
 
 		return
 	}
 
-	responseProjects := getProjectResponse(page, limit, projects)
+	responseProjects := handlers.GetProjectResponse(page, limit, projects)
 
 	writer.Header().Set("Content-Type", "application/json")
 	response, err := json.Marshal(responseProjects)
@@ -126,70 +123,6 @@ func (server *Server) projects(writer http.ResponseWriter, request *http.Request
 	}
 
 	_, _ = writer.Write(response)
-}
-
-func getProjectResponse(page, limit int, projects []projectModels.Project) projectModels.ProjectResponse {
-	projectsCount := len(projects)
-	startIndex := (page - 1) * limit
-	endIndex := startIndex + limit
-	if endIndex >= len(projects) {
-		endIndex = len(projects)
-	}
-
-	return projectModels.ProjectResponse{
-		Projects: projects[startIndex:endIndex],
-		PageInfo: projectModels.PageInfo{
-			CurrentPage:   page,
-			PageCount:     int(math.Ceil(float64(projectsCount) / float64(limit))),
-			ProjectsCount: projectsCount,
-		},
-	}
-}
-
-func handleProjects(search string) ([]projectModels.Project, error) {
-	projects, err := connector.GetProjects("https://issues.apache.org")
-	if err != nil {
-		return nil, err
-	}
-
-	var responseProjects []projectModels.Project
-
-	for _, project := range projects {
-		isCorrectName := strings.Contains(strings.ToLower(project.ProjectName), strings.ToLower(search))
-		if isCorrectName {
-			responseProject := projectModels.Project{
-				ProjectId:   project.ProjectId,
-				ProjectName: project.ProjectName,
-				Existence:   false}
-
-			responseProjects = append(responseProjects, responseProject)
-		}
-	}
-
-	return responseProjects, nil
-}
-
-func parseProjectParameters(request *http.Request) (int, int, string) {
-	limit := 20
-	page := 1
-	search := ""
-
-	limitPrm := request.URL.Query().Get("limit")
-	if len(limitPrm) != 0 {
-		limit, _ = strconv.Atoi(limitPrm)
-	}
-
-	pagePrm := request.URL.Query().Get("page")
-	if len(pagePrm) != 0 {
-		page, _ = strconv.Atoi(pagePrm)
-	}
-
-	searchPrm := request.URL.Query().Get("search")
-	if len(searchPrm) != 0 {
-		search = searchPrm
-	}
-
-	return limit, page, search
 }
 
 func (server *Server) Start() {
